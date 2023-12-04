73167588c2a533a8d28934ee3f71bb60cb92f376
tony <tonyjohncox@outlook.com>
2023-12-04 20:43:00 +1000

    day 4

---
+++ 2023/day_4/src/main.rs
@@ -0,0 +1,65 @@
+use std::collections::HashSet;
+use std::fs;
+
+const INPUT_FILE_NAME: &str = "input.txt";
+
+fn main() {
+    let game_results = get_game_results();
+    println!("Part 1: {}", part1(&game_results));
+    println!("Part 2: {}", part2(&game_results));
+}
+
+fn get_game_results() -> Vec<usize> {
+    fs::read_to_string(INPUT_FILE_NAME)
+        .unwrap()
+        .lines()
+        .map(|line| line.split_at(line.find("|").unwrap()))
+        .map(|(winning_numbers, held_numbers)| {
+            num_matching(
+                parse_numbers(&winning_numbers[9..]), // first nine chars id game
+                parse_numbers(&held_numbers[1..]),    // first character is a pipe
+            )
+        })
+        .collect()
+}
+
+fn parse_numbers(numbers: &str) -> HashSet<u32> {
+    numbers
+        .split_whitespace()
+        .map(|number| number.parse::<u32>().unwrap())
+        .collect()
+}
+
+fn num_matching(winning_numbers: HashSet<u32>, held_numbers: HashSet<u32>) -> usize {
+    winning_numbers.intersection(&held_numbers).count()
+}
+
+fn part1(game_results: &Vec<usize>) -> u32 {
+    game_results.iter().map(|game| score(*game)).sum()
+}
+
+fn part2(game_results: &Vec<usize>) -> usize {
+    // the total scratchies is the number of copies + the original set of scratchies
+    game_results
+        .iter()
+        .enumerate()
+        .fold(0, |acc, (i, game_result)| {
+            // the +1 here is just so we count the original scratchie as well as the copies it generates
+            acc + 1 + get_num_copies(*game_result, i, &game_results)
+        })
+}
+
+fn get_num_copies(game_result: usize, game_index: usize, all_games: &Vec<usize>) -> usize {
+    // recursively gets all copies for a given scratchie - may be finding copies of copies, etc
+    (game_index + 1..game_index + game_result + 1).fold(game_result, |acc, x| {
+        acc + get_num_copies(all_games[x], x, all_games)
+    })
+}
+
+fn score(game_result: usize) -> u32 {
+    if game_result == 0 {
+        0
+    } else {
+        2u32.pow((game_result - 1) as u32)
+    }
+}
